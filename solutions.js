ac3abab937cdc160542da38b221c9b2f0f6add29
DavidCJAdindu <lavishdigitalsolutions@gmail.com>
2020-05-18 21:53:11 +0100

    first challenge

---
+++ solutions.js
@@ -0,0 +1,12 @@
+/*
+1. Print the following on the console:
+- Hello World!
+- 77777
+- the result of 3 + 4
+- Fundamentals of Programming is awesome!
+- 7 / 3
+- a list of car brands
+- a person object, with a first & last name and age
+*/
+
+// Hello World
