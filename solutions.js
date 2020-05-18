5e6695d88dca80f2daf2eac37bd2dfc54cb5e5c7
DavidCJAdindu <lavishdigitalsolutions@gmail.com>
2020-05-18 22:38:59 +0100

    'q1'

--- solutions.js
+++ solutions.js
@@ -3,9 +3,22 @@
 - Hello World!
 - 77777
 - the result of 3 + 4
-- Fundamentals of Programming is awesome!
+- Javascript is awesome!
 - 7 / 3
 - a list of car brands
 - a person object, with a first & last name and age
 */
 
+console.log('Hello World!');
+console.log('77777');
+console.log(3 + 4);
+console.log('Javascript is awesome!');
+console.log(7 / 3);
+const cars = ['BMW', 'Mercesdes', 'Audi'];
+console.log(cars);
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  dob : '4-10-1980'
+};
+console.log(person);
